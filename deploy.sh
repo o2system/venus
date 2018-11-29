fff1e2a2b401087341818cbf53b02abad8201841
Nur Wachid <wachid@outlook.com>
2018-11-29 12:42:09 +0700

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-npm run prod
-git checkout -b gh-pages
+ 1539  git checkout -b gh-pages
+ 1540  npm run production
+ 1541  git add demo
+ 1542  git add demo -f
+ 1543  git status
+ 1544  git commit -m 'deploy to github pages'
+
 
