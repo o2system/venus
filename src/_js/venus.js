9a981da72447146fae863fc708b551350f539c62
Nur Wachid <wachid@outlook.com>
2018-12-09 20:49:19 +0700

    edit typo and remov post css ambigue

--- src/_js/venus.js
+++ src/_js/venus.js
@@ -1,2 +1,8 @@
-import 'popper.js';
-import 'bootstrap';
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('o2system-espresso');
+
+    require('bootstrap');
+} catch (e) {
+    console.log(e);
+}
