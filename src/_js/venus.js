b7d37bb884bb3e5bedcb16f672b54cdf8f80f064
Nur Wachid <wachid@outlook.com>
2018-12-12 21:48:18 +0700

    add main epresso

--- src/_js/venus.js
+++ src/_js/venus.js
@@ -1,8 +1,8 @@
 try {
     window.Popper = require('popper.js').default;
-    // window.$ = window.jQuery = require('o2system-espresso');
-
+    window.$ = window.jQuery = require('jquery');
     require('bootstrap');
+    require('o2system-espresso/src/espresso.js');
     // require('owl.carousel');
     require('./custom.js');
 } catch (e) {
