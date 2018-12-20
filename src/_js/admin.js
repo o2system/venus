dc558656c8cf588370321f17d16ff32f447064f5
teguhrianto22 <teguh.rianto22@gmail.com>
2018-12-20 19:09:13 +0700

    update admin

--- src/_js/admin.js
+++ src/_js/admin.js
@@ -1,9 +1,61 @@
 import metisMenu from 'metismenu';
+import niceScroll from 'nicescroll';
 
 $(document).ready(function(){
+	var sidebarLeftScroll = $('.sidebar-left').niceScroll({
+		cursorcolor: "#fff",
+		cursoropacitymax: 0.5,
+		cursorwidth: "3px"
+	});
+
+	var dropdownItemList = $('.dropdown-item-list').niceScroll({
+		cursorcolor: "#333",
+		cursoropacitymax: 0.5,
+		cursorwidth: "3px",
+		horizrailenabled:false,
+		autohidemode: false,
+	});
+
+	$('.dropdown').on("shown.bs.dropdown",function(e){
+      	dropdownItemList.show().resize();
+    });
+
+    $('.dropdown').on("hide.bs.dropdown",function(e){
+      	dropdownItemList.hide();
+    });
+
 	$('.sidebar-menu > ul').metisMenu();
 
-	$('[data-toggle="sidebar-left"]').on('click', function(){
-		$('.venus-admin').toggleClass('open-sidebar-left');
-	});
+	function sidebarLeftToggle() {
+        if ($(window).width() < 991) {
+            $('.venus-admin').removeClass('mini-sidebar-left');
+            $('[data-toggle="sidebar-left"]').on('click', function(){
+				$('.venus-admin').toggleClass('open-sidebar-left');
+			});
+       }else{
+            $('[data-toggle="sidebar-left"]').on('click', function(){
+				$('.venus-admin').toggleClass('mini-sidebar-left');
+
+				if($('.venus-admin').hasClass('mini-sidebar-left')){
+					sidebarLeftScroll.remove().hide();
+				} else {
+					$('.sidebar-left').css('overflow', 'visible');
+					setTimeout(function() {
+						$('.sidebar-left').css('overflow', 'hidden');
+						$('.sidebar-left').niceScroll({
+							cursorcolor: "#fff",
+							cursoropacitymax: 0.5,
+							cursorwidth: "3px"
+						});
+					}, 300);
+				}
+			});
+       }
+    }
+
+    sidebarLeftToggle();
+
+    $(window).resize(function() {
+       sidebarLeftToggle();
+    });
 });
